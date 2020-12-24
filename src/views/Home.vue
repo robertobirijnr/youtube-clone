16ab4cc0824e38ed9ad1fee7e93a36887b1db1bf
robertobirijnr <robertobirijnr@hotmail.com>
2020-12-24 19:19:24 +0000

    search bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <search-bar/>
     
   </div>
 </template>
 
 <script>
+import SearchBar from '../components/searchBar.vue'
 // @ is an alias to /src
 
 
 export default {
   name: 'Home',
   components: {
+    SearchBar
     
   }
 }
